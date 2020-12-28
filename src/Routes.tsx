12e0a499e1c2cbe3ad89bc006c2d4b0eec85f155
Avinash Sinha <savinash2608@gmail.com>
2020-12-28 16:57:32 +0530

    Splash inserted in Routes.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,11 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Splash } from "./components";
 const Home = lazy(() => import("./pages/Home"));
 
 export const Routes = () => (
   <Router>
-    <Suspense fallback={<div>loading..</div>}>
+    <Suspense fallback={<Splash />}>
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
