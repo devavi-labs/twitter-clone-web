6e974b7ef9bdc8b1d265122ff88d9a25aae89726
Avinash Sinha <savinash2608@gmail.com>
2020-12-28 20:48:09 +0530

    style and theme fixed

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,20 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import { TypographyOptions } from "@material-ui/core/styles/createTypography";
+
+const typography: TypographyOptions = {
+  fontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    '"Segoe UI"',
+    "Roboto",
+    '"Helvetica Neue"',
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+  ].join(","),
+};
 
 export const light = createMuiTheme({
   palette: {
@@ -10,16 +26,19 @@ export const light = createMuiTheme({
       main: "#FFFFFF",
     },
   },
+  typography,
 });
 
 export const dark = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#000000",
+      main: "#1EA1F2",
     },
     secondary: {
-      main: "#FFFFFF",
+      main: "#000000",
     },
+    tonalOffset: 0.3,
   },
+  typography,
 });
