d1c2a2b091c3b5ce5f545f0cd81cead0a33e9ab7
Avinash Sinha <savinash2608@gmail.com>
2020-12-30 23:05:43 +0530

    header removed

--- src/utils/fetchOptions.ts
+++ src/utils/fetchOptions.ts
@@ -1,9 +1,5 @@
-import { ACCESS_TOKEN } from "./constants";
-
 export const createFetchOptions = () => {
-  const accessToken = localStorage.getItem(ACCESS_TOKEN);
   return {
     credentials: "include" as const,
-    headers: { Authorization: `Bearer ${accessToken}` },
   };
 };
