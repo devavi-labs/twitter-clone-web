2907d618571022ce4a6838c6f90a062ba58d2f6a
Avinash Sinha <savinash2608@gmail.com>
2020-12-31 13:29:40 +0530

    not-working service worker removed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +12,3 @@ ReactDOM.render(
 );
 
 reportWebVitals(console.log);
-serviceWorker.register();
