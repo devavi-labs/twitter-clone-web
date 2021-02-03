72570091de6db0cbc6784874213f9ab427fd822e
Avinash Sinha <savinash2608@gmail.com>
2021-02-03 14:57:36 +0530

    stat component refactored

--- src/components/Stat/index.tsx
+++ src/components/Stat/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@material-ui/core";
 import numeral from "numeral";
 import React from "react";
+import { Link as RouteLink } from "react-router-dom";
 import { useStyles } from "./styles";
 
 type StatProps = {
@@ -12,14 +13,22 @@ type StatProps = {
 const Stat: React.FC<StatProps> = ({ label, stat, href }) => {
   const classes = useStyles();
 
-  return (
-    <Link href={href} className={classes.root}>
+  const child = (
+    <React.Fragment>
       <span className={classes.stat}>
         {numeral(stat).format(stat > 10000 ? "0 a" : "0,0") + " "}
       </span>
       <span className={classes.label}>{label}</span>
-    </Link>
+    </React.Fragment>
   );
+
+  if (href) {
+    return (
+      <Link component={RouteLink} to={href} className={classes.root}>
+        {child}
+      </Link>
+    );
+  } else return <div className={classes.root}>{child}</div>;
 };
 
 export { Stat };
