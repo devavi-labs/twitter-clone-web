d8bdca42cc14281766226740065e3b0941f81182
Avinash Sinha <savinash2608@gmail.com>
2021-02-18 12:16:29 +0530

    create quack fab only if logged in

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,23 +1,27 @@
 import { Divider } from "@material-ui/core";
 import React from "react";
 import { QuackButton } from "..";
+import { useMeQuery } from "../../generated/graphql";
 import { useStyles } from "./styles";
 
 export const Hero: React.FC = ({ children }) => {
   const classes = useStyles();
+  const [{ data }] = useMeQuery();
 
   return (
     <div className={classes.root}>
       <Divider orientation="vertical" />
       <div className={classes.subRoot}>{children}</div>
       <Divider orientation="vertical" />
-      <QuackButton
-        variant="fab"
-        fabProps={{
-          className: classes.fab,
-          size: "large",
-        }}
-      />
+      {data?.me && (
+        <QuackButton
+          variant="fab"
+          fabProps={{
+            className: classes.fab,
+            size: "large",
+          }}
+        />
+      )}
     </div>
   );
 };
